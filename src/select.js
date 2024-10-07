4fd877b17b3031f60a90a3df93dd35a921908946
Dolphi <evgeniysolomanidin@gmail.com>
2024-10-07 14:06:30 +0300

    fix: build

--- src/select.js
+++ src/select.js
@@ -130,7 +130,8 @@ export default function getQuerySelector(input, options = {}) {
   const path = (input.length && !input.name)
     ? getMultiSelectorPath(input, options)
     : getSingleSelectorPath(input, options)
-  if (Array.isArray(path) && typeof path[0] === HTMLElement) {
+
+  if (Array.isArray(path) && path.length > 0 && path[0] instanceof HTMLElement) {
     return path.map(entry => getQuerySelector(entry, options)).join(',')
   }
   return getToString(options).path(path)
